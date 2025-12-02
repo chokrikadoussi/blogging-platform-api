a8c79a9bc845b82a56092919167cc60044576e27
chokrikadoussi <kadoussi.chokri@gmail.com>
2025-12-02 13:49:17 +0100

    feat: adding notfound middleware

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const postRouter = require("./services/postService");
+const notFound = require("./middlewares/notFound");
 const app = express();
 
 app.use(express.json());
 
 app.use("/", postRouter);
+app.use(notFound);
 
 module.exports = app;
